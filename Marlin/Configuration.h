5057797c24d19cff6efa25c2b714466b080cc808
Bug38 <lguimard38@gmail.com>
2018-11-15 15:37:23 +0100

    Config for 1/16 microstepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -639,7 +639,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 200 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 200 }
 
 /**
  * Default Max Feed Rate (mm/s)
